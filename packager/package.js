d404ab44b83b98a21484fe170ab74349e18e32ca
Michael Radionov <micrddrgn@gmail.com>
2018-12-25 22:02:39 +0300

    fix(packager): package as asar

--- packager/package.js
+++ packager/package.js
@@ -9,9 +9,7 @@ const options = {
   dir: rootDir,
   out: distDir,
   arch: 'x64',
-  ignore: [
-    /node_modules/,
-  ],
+  asar: true,
   overwrite: true,
   platform: [
     'linux',
@@ -21,8 +19,10 @@ const options = {
 
 packager(options, (err, appPaths) => {
   if (err) {
+    // eslint-disable-next-line no-console
     console.error(err);
   } else {
+    // eslint-disable-next-line no-console
     console.log('Done: ', appPaths);
   }
 });
